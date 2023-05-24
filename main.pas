885049ef07f718b0ff738ab7f4d8c1c8a0969b50
Ilya Tolstenko <tolstenko-iv@ic.ntcees.ru>
2023-05-24 08:07:41 +0300

    refactoring

--- main.pas
+++ main.pas
@@ -5,12 +5,10 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Interfaces,
-  Clipbrd, ExtCtrls, Buttons,IniFiles;
-
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Clipbrd,
+  ExtCtrls, Buttons, IniFiles;
 
 type
-
   { TMainForm }
 
   TMainForm = class(TForm)
@@ -26,19 +24,23 @@ type
     procedure Timer1Timer(Sender: TObject);
     procedure ToggleBox1Change(Sender: TObject);
   private
+    ClipboardText: string;
+    PreviousClipboardText: string;
+    ConfigFileName: string;
+    FileText: string;
+    ClipboardFile: TextFile;
 
+    procedure WriteSettingsToIni(const AFileName: string; TimerEnabled: Boolean);
+    procedure LoadSettingsFromIni();
+    procedure SyncClipboardToText();
+    procedure SyncTextToClipboard();
+    procedure UpdateUI();
   public
 
   end;
 
 var
   MainForm: TMainForm;
-  ClipboardText: string;
-  PreviousClipboardText: string;
-  FileName: string;
-  FileText: string;
-  ClipboardFile: TextFile;
-
 
 implementation
 
@@ -46,22 +48,20 @@ implementation
 
 { TMainForm }
 
-
-procedure WriteSettingsToIni(const FileName: string; TimerEnabled: Boolean);
+procedure TMainForm.WriteSettingsToIni(const AFileName: string; TimerEnabled: Boolean);
 var
   IniFile: TIniFile;
 begin
   IniFile := TIniFile.Create(ChangeFileExt(ParamStr(0), '.ini'));
   try
-    IniFile.WriteString('Settings', 'FileName', FileName);
+    IniFile.WriteString('Settings', 'FileName', AFileName);
     IniFile.WriteBool('Settings', 'TimerEnabled', TimerEnabled);
   finally
     IniFile.Free;
   end;
 end;
 
-
-procedure LoadSettingsFromIni();
+procedure TMainForm.LoadSettingsFromIni();
 var
   IniFile: TIniFile;
   FileName: string;
@@ -74,99 +74,89 @@ begin
   finally
     IniFile.Free;
   end;
-  MainForm.label1.Caption := FileName;
-  MainForm.ToggleBox1.Checked :=  TimerEnabled;
+  Label1.Caption := FileName;
+  ToggleBox1.Checked := TimerEnabled;
+end;
+
+procedure TMainForm.SyncClipboardToText();
+begin
+  if Clipboard.AsText <> '' then
+  begin
+    ClipboardText := Clipboard.AsText;
+    PreviousClipboardText := ClipboardText;
+    Edit1.Text := ClipboardText;
+  end;
+end;
+
+procedure TMainForm.SyncTextToClipboard();
+begin
+  if (FileText <> Clipboard.AsText) then
+  begin
+    Clipboard.AsText := FileText;
+    Edit1.Text := FileText;
+  end;
+end;
+
+procedure TMainForm.UpdateUI();
+begin
+  if ToggleBox1.Checked then
+    ToggleBox1.Caption := 'Sync ENABLED'
+  else
+    ToggleBox1.Caption := 'Sync DISABLED';
+
+  WriteSettingsToIni(Label1.Caption, Timer1.Enabled);
 end;
 
 procedure TMainForm.Button1Click(Sender: TObject);
 begin
-  Savedialog1.Execute;
-  label1.Caption:=Savedialog1.FileName;
-  WriteSettingsToIni(label1.Caption, Timer1.Enabled)
+  if SaveDialog1.Execute then
+  begin
+    Label1.Caption := SaveDialog1.FileName;
+    UpdateUI();
+  end;
 end;
 
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
-
-
-     PreviousClipboardText := Clipboard.AsText;
-     label1.caption := '';
-     LoadSettingsFromIni();
-     ToggleBox1.OnChange(Sender);
-     if Clipboard.AsText <> '' then
-     begin
-          ClipboardText := Clipboard.AsText;
-          PreviousClipboardText := ClipboardText;
-          edit1.text := ClipboardText;
-     end;
+  PreviousClipboardText := Clipboard.AsText;
+  Label1.Caption := '';
+  LoadSettingsFromIni();
+  ToggleBox1.OnChange(Sender);
+  SyncClipboardToText();
 end;
 
 procedure TMainForm.Timer1Timer(Sender: TObject);
 var
   FileContents: TStringList;
-
 begin
-  // Мониторинг содержимого буфера обмена
-  // Если в буфере текст то записываем его в переменную.
-  if Clipboard.AsText <> '' then  ClipboardText := Clipboard.AsText;
-  // Если текст в буфере изменился
-  if ClipboardText <> PreviousClipboardText then
-     begin
-       // Выводим его содержимео в эдит-текст
-       edit1.text := ClipboardText;
-       // Сохраняем в переменную PreviousClipboardText
-       PreviousClipboardText := ClipboardText;
-       // Если имя файла не пустое то пробуем записать в этот файл
-       if label1.Caption <> '' then
-          begin
-               AssignFile(ClipboardFile, label1.Caption);
-               Rewrite(ClipboardFile);
-               Write(ClipboardFile, ClipboardText);
-               CloseFile(ClipboardFile);
-          end;
-       end;
-  // Мониторинг содержимого файла
-  if label1.Caption <> '' then
-     begin
-          FileContents := TStringList.Create;
-          try
-             FileContents.LoadFromFile(label1.Caption);
-             FileText := FileContents.Text;
-             // Костыльно пытаюсь удалить последний символ если это перевод строки
-             if Length(FileText) > 0 then
-             begin
-               if FileText[Length(FileText)] = #10 then
-               Delete(FileText, Length(FileText), 1);
-             end;
-          finally
-             FileContents.Free;
-          end;
-      // Если буфер и файл не совпадают записать файл в буфер
-      if (FileText <> Clipboard.AsText) then
+  SyncClipboardToText();
+
+  if Label1.Caption <> '' then
+  begin
+    FileContents := TStringList.Create;
+    try
+      FileContents.LoadFromFile(Label1.Caption);
+      FileText := FileContents.Text;
+
+      if Length(FileText) > 0 then
       begin
-           Clipboard.AsText := FileText;
-           edit1.text := FileText;
+        if FileText[Length(FileText)] = #10 then
+          Delete(FileText, Length(FileText), 1);
       end;
+    finally
+      FileContents.Free;
     end;
 
+    SyncTextToClipboard();
+  end;
 end;
 
 procedure TMainForm.ToggleBox1Change(Sender: TObject);
 begin
-       if ToggleBox1.Checked then
-          begin
-            ToggleBox1.Caption := 'Sync ENABLED';
-            Timer1.Enabled:= True;
-
-          end
-       else
-           begin
-             ToggleBox1.Caption := 'Sync DISABLED';
-             Timer1.Enabled:= False;
-           end;
-       WriteSettingsToIni(label1.Caption, Timer1.Enabled)
-
+  Timer1.Enabled := ToggleBox1.Checked;
+  UpdateUI();
 end;
 
 end.
+
 
